7a490ab13604a20fbe8db21c4b407e83945f64ab
Zhenia Kashperuk <pervirokim@gmail.com>
2025-10-09 13:21:50 +0300

    feat: update AI Alternative article content with new structure

--- src/components/ArticleAiAlternativeBody/ArticleAiAlternativeBody.jsx
+++ src/components/ArticleAiAlternativeBody/ArticleAiAlternativeBody.jsx
@@ -2,7 +2,6 @@
 
 import StarIcon from "@/assets/icons/starIcon.svg";
 import { useTranslations } from "@/i18n";
-import cn from "classnames";
 import Ellipse from "@/helpers/components/Ellipse/Ellipse";
 import s from "../ArticleAgencyBody/ArticleAgencyBody.module.scss";
 
@@ -26,31 +25,23 @@ export default function ArticleAiAlternativeBody() {
         <h2 className={s.chapterTitle}>{t("articleAiAlternative.chapter2.title")}</h2>
         <p className={s.p}>{t("articleAiAlternative.chapter2.p1")}</p>
         <p className={s.p}>{t("articleAiAlternative.chapter2.p2")}</p>
+        <p className={s.p}>{t("articleAiAlternative.chapter2.p3")}</p>
       </section>
 
       <section className={s.chapter}>
         <h2 className={s.chapterTitle}>{t("articleAiAlternative.chapter3.title")}</h2>
-        <ul className={s.list}>
-          <li className={s.listItem}>
-            <StarIcon className={s.starIcon} />
-            {t("articleAiAlternative.chapter3.list.0")}
-          </li>
-          <li className={s.listItem}>
-            <StarIcon className={s.starIcon} />
-            {t("articleAiAlternative.chapter3.list.1")}
-          </li>
-          <li className={s.listItem}>
-            <StarIcon className={s.starIcon} />
-            {t("articleAiAlternative.chapter3.list.2")}
-          </li>
-        </ul>
         <p className={s.p}>{t("articleAiAlternative.chapter3.p1")}</p>
+        <p className={s.p}>{t("articleAiAlternative.chapter3.p2")}</p>
+        <div className={s.cardFilled}>
+          <p className={s.lead}>{t("articleAiAlternative.chapter3.p3")}</p>
+        </div>
       </section>
 
       <section className={s.chapter}>
         <h2 className={s.chapterTitle}>{t("articleAiAlternative.chapter4.title")}</h2>
+        <p className={s.p}>{t("articleAiAlternative.chapter4.p1")}</p>
         <div className={s.cardBordered}>
-          <p className={s.p}>{t("articleAiAlternative.chapter4.p1")}</p>
+          <p className={s.p}>{t("articleAiAlternative.chapter4.p2")}</p>
         </div>
       </section>
 
@@ -75,36 +66,45 @@ export default function ArticleAiAlternativeBody() {
         <h2 className={s.chapterTitle}>{t("articleAiAlternative.chapter6.title")}</h2>
         <p className={s.p}>{t("articleAiAlternative.chapter6.p1")}</p>
         <p className={s.p}>{t("articleAiAlternative.chapter6.p2")}</p>
+        <p className={s.p}>{t("articleAiAlternative.chapter6.p3")}</p>
+      </section>
+
+      <section className={s.chapter}>
+        <h2 className={s.chapterTitle}>{t("articleAiAlternative.chapter7.title")}</h2>
+        <p className={s.p}>{t("articleAiAlternative.chapter7.p1")}</p>
+        <div className={s.cardBordered}>
+          <p className={s.p}>{t("articleAiAlternative.chapter7.p2")}</p>
+        </div>
       </section>
 
       <section className={s.chapter}>
         <div className={s.cardFilled}>
-          <p className={s.lead}>{t("articleAiAlternative.chapter7.title")}</p>
+          <p className={s.lead}>{t("articleAiAlternative.chapter8.title")}</p>
           <ul className={s.list}>
             <li className={s.listItem}>
               <StarIcon className={s.starIcon} />
-              {t("articleAiAlternative.chapter7.list.0")}
+              {t("articleAiAlternative.chapter8.list.0")}
             </li>
             <li className={s.listItem}>
               <StarIcon className={s.starIcon} />
-              {t("articleAiAlternative.chapter7.list.1")}
+              {t("articleAiAlternative.chapter8.list.1")}
             </li>
             <li className={s.listItem}>
               <StarIcon className={s.starIcon} />
-              {t("articleAiAlternative.chapter7.list.2")}
+              {t("articleAiAlternative.chapter8.list.2")}
             </li>
             <li className={s.listItem}>
               <StarIcon className={s.starIcon} />
-              {t("articleAiAlternative.chapter7.list.3")}
+              {t("articleAiAlternative.chapter8.list.3")}
             </li>
             <li className={s.listItem}>
               <StarIcon className={s.starIcon} />
-              {t("articleAiAlternative.chapter7.list.4")}
+              {t("articleAiAlternative.chapter8.list.4")}
             </li>
           </ul>
+          <p className={s.p}>{t("articleAiAlternative.chapter8.p1")}</p>
         </div>
       </section>
     </div>
   );
 }
-
